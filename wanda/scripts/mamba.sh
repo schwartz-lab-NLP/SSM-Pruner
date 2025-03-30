6449476a028c519a6514f5e37c76799f202f71da
tamer <tammer.ghattas@gmail.com>
2025-03-30 23:09:44 +0300

    updated wanda run script

--- wanda/scripts/mamba.sh
+++ wanda/scripts/mamba.sh
@@ -3,7 +3,8 @@
 # Set common variables
 #model="goombalab/Phi-Mamba"
 #model="state-spaces/mamba2-2.7b"
-model="schwartz-lab/Smol2-Mamba-1.9B"
+# model="schwartz-lab/Smol2-Mamba-1.9B"
+model="HuggingFaceTB/SmolLM2-1.7B"
 #model="JunxiongWang/Llama3.2-Mamba2-3B-dpo"
 
 cuda_device=0
@@ -13,6 +14,7 @@ export CUDA_VISIBLE_DEVICES=$cuda_device
 export PYTHONPATH=".:$PYTHONPATH"
 export PYTHONPATH="./phi_mamba:$PYTHONPATH"
 export PYTHONPATH="./MambaInLlama:$PYTHONPATH"
+export PYTHONPATH="./wanda:$PYTHONPATH"
 
 # Define function to run python command
 run_python_command () {
@@ -22,9 +24,9 @@ run_python_command () {
     --sparsity_ratio $5 \
     --sparsity_type $2 \
     --save $3 \
-    --is_mamba \
-    --is_lm_head \
-#    --save_model $4 \
+    --save_model $4
+    # --is_mamba \
+    # --is_lm_head \
 #    --is_mamba_in_llama \
 #    --eval_zero_shot \
 #    --lm_eval_name auto-lm-head
@@ -50,7 +52,10 @@ run_python_command () {
 #echo "--------------------------------------------------------------------------------------"
 #run_python_command "wanda" "unstructured" "out/mamba/unstructured/wanda/phi_05" "out/mamba/unstructured/wanda/phi_05/model/" 0.5
 
-run_python_command "wanda" "unstructured" "out/mamba/unstructured/wanda/SMOL19_025" "out/mamba/unstructured/wanda/SMOL19_05/model/" 0.5
+# run_python_command "wanda" "unstructured" "out/mamba/unstructured/wanda/SMOL19_025" "out/mamba/unstructured/wanda/SMOL19_05/model/" 0.5
+
+run_python_command "wanda" "unstructured" "out/mamba/unstructured/wanda/SMOL17_025" "out/mamba/unstructured/wanda/SMOL17_025/model/" 0.25
+# run_python_command "wanda" "unstructured" "out/mamba/unstructured/wanda/SMOL17_05" "out/mamba/unstructured/wanda/SMOL17_05/model/" 0.5
 
 
 

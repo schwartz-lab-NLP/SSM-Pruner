eb235f4910dff81e1c3f5c78f9953c918323c027
tamer <tamerg@ai21.com>
2025-05-20 13:26:49 +0300

    added llamba eval

--- phi_mamba/lm_harness_eval.py
+++ phi_mamba/lm_harness_eval.py
@@ -140,6 +140,19 @@ class Smol17LMheadEvalWrapper(BaseEvalWrapper):
         super().__init__(_model, tokenizer="HuggingFaceTB/SmolLM2-1.7B", **kwargs)
         self._model.to(self._device).to(self._dtype).eval()
 
+@register_model("lm-head-llamba1b")
+class Smol17LMheadEvalWrapper(BaseEvalWrapper):
+    AUTO_MODEL_CLASS = transformers.AutoModelForCausalLM
+
+    def __init__(self, pretrained=None, peft=None, **kwargs):
+        path = os.environ.get("EVAL_PATH", ".")
+        
+        _model = LMHeadModel.from_pretrained(path, strict=True)
+        if peft is not None:
+            _model = PeftModel.from_pretrained(_model, peft)
+            print('PEFT PATH IN USE ------------------->', peft)
+        super().__init__(_model, tokenizer="meta-llama/Llama-3.2-1B", **kwargs)
+        self._model.to(self._device).to(self._dtype).eval()
 
 @register_model("lm-head-smol17-hf")
 class Smol17LMheadHFEvalWrapper(BaseEvalWrapper):
